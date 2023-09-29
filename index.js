7eb4d28cdef2b5e5e903316992888737255faa84
Alfonso <alfonsobanzon@gmail.com>
2023-09-29 00:51:32 +0000

    now prints time in CUT

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-const { fetchMyIP } = require("./iss");
-const { fetchCoordsByIP } = require("./iss");
-const { fetchISSFlyOverTimes } = require("./iss");
+// const { fetchMyIP } = require("./iss");
+// const { fetchCoordsByIP } = require("./iss");
+// const { fetchISSFlyOverTimes } = require("./iss");
+
 const { nextISSTimesForMyLocation } = require("./iss");
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
 
 nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
     return console.log("It didn't work!", error)
   }
 
-  console.log("Next 5 ISS flyover times:", passTimes)
+  printPassTimes(passTimes);
 })
 
 // fetchMyIP((error, ip) => {
